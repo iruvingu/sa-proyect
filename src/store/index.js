2380174fb2bcc1b65f95cf413e5fc737452fdf73
Irving Flores <rfloresg1002@alumno.ipn.mx>
2019-01-15 18:02:16 -0600

    [EDITED] Store enhancer was edited

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,10 @@ import storage from 'redux-persist/lib/storage'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||compose;
 
+const enhancer = composeEnhancers(
+  applyMiddleware(Thunk)
+)
+
 const persistConfig = {
   key: 'root',
   storage: storage,
@@ -14,6 +18,6 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers)
 
-export const store = createStore(persistedReducer, {}, composeEnhancers(applyMiddleware(Thunk)))
+export const store = createStore(persistedReducer, {}, enhancer)
 
 export const persistor = persistStore(store)
